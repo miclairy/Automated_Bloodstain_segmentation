95db3a91b6fb701f10b7badf37d16d86cabcd045
Claire Barnaby <cba62@uclive.ac.nz>
2018-04-22 19:31:44 +1200

    hsv play
    Former-commit-id: dda865e1f3524f6b0e6ac2be39d618b45ad7d036

--- binary_image.py
+++ binary_image.py
@@ -1,5 +1,3 @@
-# hough_circle.py
-
 import cv2
 import numpy as np
 
@@ -8,18 +6,42 @@ def nothing(x):
     # It doesn't need to do anything, however.
     pass
 
-img_original = cv2.imread('./images/Panorama 73 cropped.tif')
+img_original = cv2.imread('/home/cosc/student/cba62/Documents/stain segmentation matlab code/images/cast_off_5.tif')
 height,width = img_original.shape[:2]
 
-# Convert the image to grayscale for processing
-gray = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
+lower_hue = 0.0 / 360
+upper_hue = 360.0 / 360
+saturation = 0.2
+value = 0.1
 
+lower_threshold = np.array([lower_hue * 179, saturation * 255, value * 255])
+upper_threshold = np.array([upper_hue * 179, 255, 255])
+
+hsv_img = cv2.cvtColor(img_original, cv2.COLOR_BGR2HSV)
+
+col_thresh = cv2.inRange(hsv_img, lower_threshold, upper_threshold)
+
+kernel = np.ones((3,3),np.uint8)
+erosion = cv2.erode(col_thresh,kernel,iterations = 2)
+dilation = cv2.dilate(erosion, kernel, iterations = 2)
+
+# Convert the image to grayscale for processing
+# gray = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
+#
+# edges = cv2.Canny(gray, 600, 600)
+#
+# gray_blur = cv2.medianBlur(gray, 51)
+#
+# ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+# diff = cv2.subtract(thresh, gray)
 
 while True:
-    gray_blur = cv2.medianBlur(gray, 5)
-    # thresh = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 1)
-    # small = cv2.resize(gray_blur, (0,0), fx=0.5, fy=0.5)
-    small = cv2.pyrDown(img_original, dstsize=(width // 4, height // 4))
+
+    small = cv2.resize(hsv_img, (0,0), fx=0.125, fy=0.125)
+    # small = cv2.pyrDown(gray, dstsize=(width // 2, height // 2))
+   # cv2.imshow('Blood Spatter', small)
+    small = cv2.resize(dilation, (0,0), fx=0.125, fy=0.125)
+
     cv2.imshow('Blood Spatter', small)
 
     if cv2.waitKey(100) & 0xFF == ord('q'):
