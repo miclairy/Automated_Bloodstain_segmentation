8c0660d129b5145ec272ed0ed5acac0055f81687
Claire Barnaby <cba62@uclive.ac.nz>
2018-04-26 21:05:05 +1200

    area ellipses
    Former-commit-id: 9a9e9ee4e17f1c33291f2c33ab76ae0cd1793cb9

--- binary_image.py
+++ binary_image.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+
 
 # img_original = cv2.imread('./images/Panorama 64 cropped.tif')
 # img_original = cv2.imread('./images/211 Rep 4 MAX.JPG')
-img_original = cv2.imread('./images/Impact 5.tif')
+img_original = cv2.imread('./images/cast_off_5.tif')
+# img_original = cv2.imread('/home/cosc/student/cba62/Documents/COSC428/lab02/images/windows.jpg')
 
 height,width = img_original.shape[:2]
 
@@ -21,12 +24,20 @@ hsv_img = cv2.cvtColor(img_original, cv2.COLOR_BGR2HSV)
 
 col_thresh = cv2.inRange(hsv_img, lower_threshold, upper_threshold)
 
+blur = cv2.GaussianBlur(img_original, (9,9), 0)
+# Convert the image to grayscale for processing
+gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+
+# edges = cv2.Canny(gray, 66, 100)
+# ret3,th3 = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
 kernel = np.ones((3,3),np.uint8)
-erosion = cv2.erode(col_thresh,kernel,iterations = 2)
+erosion = cv2.erode(col_thresh ,kernel,iterations = 2)
 dilation = cv2.dilate(erosion, kernel, iterations = 2)
+
 ret, labels = cv2.connectedComponents(dilation)
 
-# make pretty colours
+# # make pretty colours
 label_hue = np.uint8(179 * labels / np.max(labels))
 blank_ch = 255 * np.ones_like(label_hue)
 labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
@@ -35,24 +46,19 @@ labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
 
 labeled_img[label_hue==0] = 0
 
-blur = cv2.GaussianBlur(img_original, (9,9), 0)
-# Convert the image to grayscale for processing
-gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
 
 
-#
-# edges = cv2.Canny(gray, 600, 600)
 #
 # gray_blur = cv2.medianBlur(gray, 51)
 #
 # ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 # diff = cv2.subtract(thresh, gray)
 
-# 
-
-    # small = cv2.pyrDown(gray, dstsize=(width // 2, height // 2))
-   # cv2.imshow('Blood Spatter', small)
+# small = cv2.pyrDown(gray, dstsize=(width // 2, height // 2))
+# cv2.imshow('Blood Spatter', small)
    
+hist = cv2.calcHist( [hsv_img], [0, 1], None, [180, 256], [0, 180, 0, 256] )
+
 
 circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, param1=100,
                         param2=58, minRadius=24, maxRadius=82)
@@ -68,10 +74,21 @@ if circles is not None:
         #  cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
 
 im2, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(img_original, contours, -1, (255,0,0), 3)
+# cv2.drawContours(img_original, contours, -1, (255,0,0), 3)
+
+area = 0
+for cnt in contours:
+    area += cv2.contourArea(cnt)
+    if len(cnt) >= 5:
+        ellipse = cv2.fitEllipse(cnt)
+        cv2.ellipse(img_original, ellipse, (0,255,0), 2)
+print("mean area:", area / len(contours))
 
 while True:
-    small = cv2.resize(img_original, (0,0), fx=0.15, fy=0.15)
+
+    #plt.imshow(hist,interpolation = 'nearest')
+   # plt.show()
+    small = cv2.resize(img_original, (0,0), fx=0.25, fy=0.25)
     cv2.imshow('Blood Spatter', small)
     
     if cv2.waitKey(100) & 0xFF == ord('q'):
