32e954f708f53709f8997833b7e269a1b03523e8
Claire Barnaby <cba62@uclive.ac.nz>
2018-06-21 17:24:05 +1200

    added stain object for putting properties on
    Former-commit-id: ed5cda0df228e25ded81e00154bdd41e55e43e1a

---
+++ image processing v1/bloodstain.py
@@ -0,0 +1,24 @@
+import cv2
+
+class Stain:
+
+    def __init__(self, contour):
+        self.contour = contour
+        self.ellipse = cv2.fitEllipse(self.contour) if len(self.contour) >= 5 else None
+        self.area = cv2.contourArea(self.contour)  
+        self.circularity = -1
+        self.regularity = -1
+        self.lightness = -1
+        self.orientaton =  -1
+        self.directionality = -1       
+
+    def draw_ellipse(self, img_original):
+        if self.ellipse is not None:
+            cv2.ellipse(img_original, self.ellipse, (0,255,0), 2)
+            
+    
+    def annotate(self):
+
+        x,y,w,h = cv2.boundingRect(self.contour)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img_original, str(self.area) + " width: " + str(w) + "height: " + str(h), (int(x + 10), int(y + 30)), font, 1, (0,255,255), 2, cv2.LINE_AA)
