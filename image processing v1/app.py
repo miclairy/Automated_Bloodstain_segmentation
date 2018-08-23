fd20de05326eb0e800d5e766678e16c78f73c791
Claire Barnaby <cba62@uclive.ac.nz>
2018-08-24 10:00:31 +1200

    started exporting
    Former-commit-id: a84e1a7718da7a1d1ac95f735e9d556dd561e62e

--- image processing v1/app.py
+++ image processing v1/app.py
@@ -6,6 +6,7 @@ from photo_viewer import PhotoViewer
 import stain_segmentation as Seg
 import cv2
 from PIL import Image
+import os
 
 class BPA_App(QtGui.QMainWindow, main_window.Ui_MainWindow):
     def __init__(self, parent=None):
@@ -15,9 +16,11 @@ class BPA_App(QtGui.QMainWindow, main_window.Ui_MainWindow):
         self.viewer.setMinimumSize(500, 500)
         self.horizontalLayout.addWidget(self.viewer)
         self.actionLoad.triggered.connect(self.load_image)
+        self.actionExport.triggered.connect(self.export)
         self.actionSegment_Image.triggered.connect(self.segment_image)
         self.file_name = ""
         self.populate_table()
+        self.result = None
 
     def load_image(self):
         self.file_name = QtGui.QFileDialog.getOpenFileName(self, 'Open file', 
@@ -25,12 +28,20 @@ class BPA_App(QtGui.QMainWindow, main_window.Ui_MainWindow):
         if self.file_name:
             self.viewer.setPhoto(pixmap=QtGui.QPixmap(self.file_name))
 
+    def export(self):
+        save_path = self.file_name
+        Seg.export_stain_data(save_path)
+        # Seg.export_pattern_data(save_path)
+        save_path = os.path.splitext(save_path)[0]
+        cv2.imwrite(save_path + "-result.jpg", self.result)
+
     def segment_image(self):
         if self.file_name != "":
             image = cv2.imread(str(self.file_name))
             orginal = cv2.imread(str(self.file_name))
-            result = Seg.stain_segmentation(image, orginal)
-            height, width, byteValue = result.shape
+            self.result = Seg.stain_segmentation(image, orginal)
+            result = self.result
+            height, width, byteValue = self.result.shape
             bytesPerLine = 3 * width
             cv2.cvtColor(result, cv2.COLOR_BGR2RGB, result)
             qImg = QtGui.QImage(result.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888)
